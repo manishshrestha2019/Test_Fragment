cbefe632f1562473179697dfc3419d359f30e6d4
Manish Shrestha <manish.sh2071@gmail.com>
2019-04-22 10:47:45 +0545

    22nd April

---
+++ app/src/main/java/com/e/test_fragment/ViewPagerActivity.java
@@ -0,0 +1,34 @@
+package com.e.test_fragment;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import adapter.ViewPagerAdapter;
+import fragment.AddFragment;
+import fragment.SecondFragment;
+
+public class ViewPagerActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+
+        tabLayout=findViewById(R.id.tabId);
+        viewPager=findViewById(R.id.viewPager);
+
+        ViewPagerAdapter adapter=new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new AddFragment(),"SUM");
+        adapter.addFragment(new SecondFragment(),"Area of Circle");
+    }
+
+}
